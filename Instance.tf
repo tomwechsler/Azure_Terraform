0bf490b76f66c55d4b646ddbbeba86454e1e9fdb
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-05-23 13:56:14 +0200

    Add files via upload

---
+++ Instance.tf
@@ -0,0 +1,31 @@
+resource "azurerm_container_group" "vote-aci" {
+  name                = "vote-aci"
+  location            = azurerm_resource_group.vote-resource-group.location
+  resource_group_name = azurerm_resource_group.vote-resource-group.name
+  ip_address_type     = "public"
+  dns_name_label      = "vote-aci"
+  os_type             = "linux"
+
+  container {
+    name   = "vote-aci"
+    image  = "microsoft/azure-vote-front:cosmosdb"
+    cpu    = "0.5"
+    memory = "1.5"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+
+    secure_environment_variables = {
+      "COSMOS_DB_ENDPOINT"  = azurerm_cosmosdb_account.vote-cosmos-db.endpoint
+      "COSMOS_DB_MASTERKEY" = azurerm_cosmosdb_account.vote-cosmos-db.primary_master_key
+      "TITLE"               = "Tom's Azure Voting App"
+      "VOTE1VALUE"          = "Cats"
+      "VOTE2VALUE"          = "Dogs"
+    }
+  }
+}
+
+output "dns" {
+  value = azurerm_container_group.vote-aci.fqdn
+}
