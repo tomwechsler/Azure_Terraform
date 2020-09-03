eeca957c19b7ff98ab5d184773e89a80fdb3849e
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2020-09-03 14:52:14 +0200

    Create main.tf

---
+++ 03_Dependencies/main.tf
@@ -0,0 +1,111 @@
+locals {
+  system = "twterrademo"
+}
+
+provider "azurerm" {
+  version = "1.38.0"
+  
+}
+
+#create resource group
+resource "azurerm_resource_group" "rg" {
+    name     = "rg-${local.system}"
+    location = "westeurope"
+    tags      = {
+      Environment = local.system
+    }
+}
+
+#Create virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "vnet-${local.system}-westeurope-001"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westeurope"
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "snet-${local.system}-westeurope-001 "
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.0.0.0/24"
+}
+
+# Create public IP
+resource "azurerm_public_ip" "publicip" {
+  name                = "pip-${local.system}-dev-westeurope-001"
+  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+
+# Create network security group and rule
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-sshallow-001 "
+  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "nic" {
+  name                      = "nic-01-${local.system}-dev-001 "
+  location                  = "westeurope"
+  resource_group_name       = azurerm_resource_group.rg.name
+  network_security_group_id = azurerm_network_security_group.nsg.id
+
+  ip_configuration {
+    name                          = "niccfg-${local.system}"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+# Create virtual machine
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "vmterraform"
+  location              = "westeurope"
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_B1s"
+
+  storage_os_disk {
+    name              = "stvmpmvmterraformos"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04.0-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "vmterraform"
+    admin_username = "terrauser"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
+#https://learn.hashicorp.com/tutorials/terraform/azure-dependency
